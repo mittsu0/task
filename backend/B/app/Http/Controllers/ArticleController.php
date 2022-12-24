0f975e79a1a4d1ef59657d121c9d6e1004469b79
mittsu0 <threebridge3824@gmail.com>
2022-12-24 13:57:29 +0900

    [add]フォームリクエストを追加

--- backend/B/app/Http/Controllers/ArticleController.php
+++ backend/B/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -34,7 +35,7 @@ class ArticleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Article $article)
+    public function store(ArticleRequest $request, Article $article)
     {
         $article->body = $request->body;
         $article->user_id = $request->user()->id;
