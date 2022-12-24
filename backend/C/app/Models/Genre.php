09c46fafde7935aaef68e85a725d4ee3d6b7478b
mittsu0 <threebridge3824@gmail.com>
2022-12-24 18:52:16 +0900

    [add]itemsテーブルとgenresテーブルのマイグレーションを追加

--- backend/C/app/Models/Genre.php
+++ backend/C/app/Models/Genre.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
+class Genre extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'price', 'genre_id'];
+    protected $fillable = ['name'];
     const CREATED_AT = null;
     const UPDATED_AT = null;
 }
