011beaedcd1960bba23ddf82b1997f35495b320a
mittsu0 <threebridge3824@gmail.com>
2022-12-24 19:01:32 +0900

    [add]itemsとgenresのfactoryを追加

--- backend/C/database/seeders/DatabaseSeeder.php
+++ backend/C/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
+use App\Models\Genre;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Genre::factory(10)->create();
+        Item::factory(100)->create();
     }
 }
