f1bdc897f685116df00c615628267f1c9efd48b6
mittsu0 <threebridge3824@gmail.com>
2022-12-24 12:10:32 +0900

    [add]一覧表示用のviewとルーティングを追加

--- backend/B/routes/web.php
+++ backend/B/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
